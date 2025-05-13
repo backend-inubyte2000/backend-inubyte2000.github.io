59f74dcbd93b75cb295a260dc902eb80a64258a9
inubyte2000 <inubyte2000@yahoo.com>
2025-05-13 15:32:47 -0400

    Add files via upload

--- tvnet/v2/back.js
+++ tvnet/v2/back.js
@@ -10,14 +10,14 @@ function getchannels() {
         ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch7.gif"],
         ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5f4d86f519358a00072b978e/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c265961-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=71f86833-3dd6-41ac-a9b8-4a4ec8873306", "https://backend-inubyte2000.github.io/tvnet/v2/ch8.gif"],
         ["https://master.nhkworld.jp/nhkworld-tv/playlist/live.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch9.gif"],
-        ["http://vthanh.utako.moe/NHK_G/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch10.gif"],
-        ["http://vthanh.utako.moe/NHK_E/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch11.gif"],
-        ["http://vthanh.utako.moe/Nippon_TV/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch12.gif"],
-        ["http://vthanh.utako.moe/TV_Asahi/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch13.gif"],
-        ["https://tbs5.mov3.co/hls/tbs.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch14.gif"],
-        ["http://vthanh.utako.moe/TV_Tokyo/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch15.gif"],
-        ["https://fujitv4.mov3.co/hls/fujitv.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch16.gif"],
-        ["http://vthanh.utako.moe/Tokyo_MX1/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch17.gif"]
+        ["https://www.rtp.pt/play/direto/rtp1", "https://backend-inubyte2000.github.io/tvnet/v2/ch10.gif"],
+        ["https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch11.gif"],
+        ["https://player-tvcultura.stream.uol.com.br/live/tvcultura.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch12.gif"],
+        ["https://www.youtube.com/embed/mBvpA1gOUos", "https://backend-inubyte2000.github.io/tvnet/v2/ch13.gif"],
+        ["https://www.rtp.pt/play/direto/rtpmemoria", "https://backend-inubyte2000.github.io/tvnet/v2/ch14.gif"],
+        ["https://video-auth2.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch15.gif"],
+        ["https://video09.logicahost.com.br/redebrasiloficial/redebrasiloficial/chunklist_w397838706.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch16.gif"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f12136385bccc00070142ed/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-inubyte2000.github.io/tvnet/v2/ch17.gif"]
     ];
     return channels;
 }
