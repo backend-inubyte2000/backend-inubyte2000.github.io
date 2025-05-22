b0cc75420960c1b8ceef5c7a83518a401ad88d74
inubyte2000 <inubyte2000@yahoo.com>
2025-05-21 22:26:10 -0400

    Add files via upload

--- tvnet/v2/back.js
+++ tvnet/v2/back.js
@@ -8,16 +8,16 @@ function getchannels() {
         ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch5.png"],
         ["https://retroblast.tv/hls/stream.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch6.png"],
         ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch7.png"],
-        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch8.png"],
-        ["http://mytv.dothome.co.kr/ch/catv/7.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch9.png"],
-        ["http://mytv.dothome.co.kr/ch/catv/1.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch10.png"],
-        ["http://mytv.dothome.co.kr/ch/catv/3.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch11.png"],
-        ["https://ebsonair.ebs.co.kr/groundwavefamilypc/familypc1m/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch12.png"],
-        ["https://ebsonair.ebs.co.kr/plus2familypc/familypc1m/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch13.png"],
-        ["http://www.hwado.net/webtv/catv/502_76142D8F.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch14.png"],
-        ["http://www.hwado.net/webtv/catv/503_CFEA7803.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch15.png"],
-        ["http://www.hwado.net/webtv/catv/52_440DDPPJ.php", "https://backend-inubyte2000.github.io/tvnet/v2/ch16.png"],
-        ["https://tv.nknews.org/tvhls/stream.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch17.png"]
+        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5cb0cae7a461406ffe3f5213/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a7819-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=8a4a0712-cb24-4d05-b429-1b98c1f73f74", "https://backend-inubyte2000.github.io/tvnet/v2/ch8.png"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch9.png"],
+        ["https://www.rtp.pt/play/direto/rtp1", "https://backend-inubyte2000.github.io/tvnet/v2/ch10.png"],
+        ["https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch11.png"],
+        ["https://d277k9d1h9dro4.cloudfront.net/out/v1/293e7c3464824cbd8818ab8e49dc5fe9/index.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch12.png"],
+        ["https://video-auth2.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch13.png"],
+        ["https://player-tvcultura.stream.uol.com.br/live/tvcultura.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch14.png"],
+        ["https://cdn.jmvstream.com/w/LVW-10842/LVW10842_513N26MDBL/chunklist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch15.png"],
+        ["https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch16.png"],
+        ["https://video09.logicahost.com.br/redebrasiloficial/redebrasiloficial/playlist.m3u8", "https://backend-inubyte2000.github.io/tvnet/v2/ch17.png"]
     ];
     return channels;
 }
