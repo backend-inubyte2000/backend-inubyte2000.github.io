3312ae2b4f984ef43c072ab8cfe0c0a5093ac1cc
inubyte2000 <inubyte2000@yahoo.com>
2025-05-29 22:17:31 -0400

    Add files via upload

--- atv/back.js
+++ atv/back.js
@@ -1,5 +1,7 @@
 ﻿function getchannels() {
     let channeldb = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "https://www.videouniversity.com/files/smpte2.gif"],
+        ["https://www.youtube.com/embed/R3GfuzLMPkA", "https://static.wikia.nocookie.net/logo-timeline/images/2/25/Youtubeicon.jpg/revision/latest?cb=20210325025222"],
         ["NULL", "NULL"]
     ];
     return channeldb;
