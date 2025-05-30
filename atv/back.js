dd4061333baf52af342430dbef07c6193aa0db17
inubyte2000 <inubyte2000@yahoo.com>
2025-05-29 22:24:40 -0400

    Add files via upload

--- atv/back.js
+++ atv/back.js
@@ -5,4 +5,34 @@
         ["NULL", "NULL"]
     ];
     return channeldb;
+}function docom() {
+    needupdate = checkupdate();
+    if (needupdate == 1) {
+        url = geturl();
+        alert = ("An update is required please press ok to get the latest update");
+        document.location = url;
+    }
+    if (needupdate == 0) {
+        const mainui = document.getElementById("mainui").innerHTML = '';
+        channels = [];
+        channels = getchannels();
+        for (var i = 0; i < channels.length; i++) {  
+            if (channels[i][0] == "NULL") {
+                const mainui = document.getElementById("mainui").innerHTML += '<div class="channel"  onmouseover="hoverfx()"></div>';
+            }
+            else {
+                const mainui = document.getElementById("mainui").innerHTML += '<div class="channel"  onmouseover="hoverfx()"><a href="'  + channels[i][0] +  '"><img src="'  + channels[i][1] +  '" class="logo"  onmouseover="hoverfx()"></a></div>';
+            }
+        }
+    }
+}
+
+function checkupdate() {
+    needupdate = 1;
+    return needupdate;
+}
+
+function geturl() {
+    url = "https://www.youtube.com/watch?v=r8E0CbYZcjE"
+    return url
 }
