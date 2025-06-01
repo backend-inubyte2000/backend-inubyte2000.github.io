bff5ab80eab8157b1422757fa0ce9baa57c6b761
inubyte2000 <inubyte2000@yahoo.com>
2025-06-01 00:00:24 -0400

    Add files via upload

--- tvazul/v0/back.js
+++ tvazul/v0/back.js
@@ -1,23 +1,16 @@
 ﻿function getchannels() {
     let channeldb = [
-        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-inubyte2000.github.io/tvazul/v0/banners/bobross.svg"],
-        ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/tvazul/v0/banners/newsnation.svg"],
-        ["http://v2.weatherscan.net/", "https://backend-inubyte2000.github.io/tvazul/v0/banners/weather.svg"],
-        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-inubyte2000.github.io/tvazul/v0/banners/tnt.svg"],
-        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5cb0cae7a461406ffe3f5213/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a7819-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=8a4a0712-cb24-4d05-b429-1b98c1f73f74", "https://backend-inubyte2000.github.io/tvazul/v0/banners/paramount.svg"],
-        ["https://fl3.moveonjoy.com/FREE_FORM/index.m3u8", "https://backend-inubyte2000.github.io/tvazul/v0/banners/freeform.svg"],
-        ["https://retroblast.tv/hls/stream.m3u8", "https://backend-inubyte2000.github.io/tvazul/v0/banners/cn.png"],
-        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-inubyte2000.github.io/tvazul/v0/banners/awe.png"],
+
     ];
     return channeldb;
 }
 
 function checkupdate() {
-    needupdate = 0;
+    needupdate = 1;
     return needupdate;
 }
 
 function geturl() {
-    url = "";
+    url = "https://github.com/inubyte2000/tvazul/archive/refs/tags/1.0.zip";
     return url
 }
