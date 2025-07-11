64738c1759efbb49b92d44cdcefa902fe02753e0
inubyte2000 <inubyte2000@yahoo.com>
2025-07-11 17:45:55 -0400

    Add files via upload

--- ieyasu/v0/back.js
+++ ieyasu/v0/back.js
@@ -1,10 +1,10 @@
 ﻿function getchannels() {
     let channeldb = [
         ["POOF", "POOF"],
-        ["NULL", "NULL"],
+        ["POOF", "NULL"],
         ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-inubyte2000.github.io/ieyasu/v0/ch2.gif"],
         ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch3.gif"],
-        ["http://v2.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.svg"],
+        ["http://v2.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.gif"],
     ];
     return channeldb;
 }
