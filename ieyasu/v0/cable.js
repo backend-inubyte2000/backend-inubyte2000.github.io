c3f1804eb21628a45d33fa662c16359c2b040b3a
inubyte2000 <inubyte2000@yahoo.com>
2025-07-11 16:32:12 -0400

    Add files via upload

---
+++ ieyasu/v0/cable.js
@@ -0,0 +1,23 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-inubyte2000.github.io/tvazul/v1/banners/bobross.svg"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/tvazul/v1/banners/newsnation.svg"],
+        ["index2.html#http://v1.weatherscan.net/", "https://backend-inubyte2000.github.io/tvazul/v1/banners/weather.svg"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
