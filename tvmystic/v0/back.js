4e35fc4e4586df65634ab43a183bde0f0544e971
inubyte2000 <inubyte2000@yahoo.com>
2025-07-12 12:15:23 -0400

    Add files via upload

--- tvmystic/v0/back.js
+++ tvmystic/v0/back.js
@@ -1,1 +1,24 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["POOF", "POOF"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvmystic/v0/cable.js
@@ -0,0 +1,27 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
