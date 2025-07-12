34d7028b2b7e163e3d9be30fe5f9ae3f00162c78
inubyte2000 <inubyte2000@yahoo.com>
2025-07-12 14:09:16 -0400

    Add files via upload

--- tvmystic/v1/back.js
+++ tvmystic/v1/back.js
@@ -4,7 +4,7 @@
         ["#", "nochan.gif"],
         ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-inubyte2000.github.io/ieyasu/v0/ch2.gif"],
         ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch3.gif"],
-        ["http://v2.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.gif"],
+        ["http://v1.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.gif"],
         ["#", "nochan.gif"],
         ["#", "nochan.gif"],
         ["https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch7.gif"],
