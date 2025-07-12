fb31741afd5cc0b0d0d345408a51b3bcc4846c94
inubyte2000 <inubyte2000@yahoo.com>
2025-07-12 12:11:37 -0400

    Add files via upload

--- ieyasu/v0/cable.js
+++ ieyasu/v0/cable.js
@@ -7,7 +7,7 @@
         ["index2.html#http://v1.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.gif"],
         ["index3.html#", "nochan.gif"],
         ["index3.html#", "nochan.gif"],
-        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch7.gif"],
+        ["index3.html#", "nochan.gif"],
         ["index3.html#", "nochan.gif"],
         ["index.html", "NULL"],
     ];
@@ -21,10 +21,6 @@ function getchinfo() {
         ["Bob Ross", "24/7 Joy of Painting"],
         ["News Nation", "Non bias news 24/7"],
         ["Weather Scan", "24/7 Weather Service"],
-        ["Channel Name", "Channel Description"],
-        ["Channel Name", "Channel Description"],
-        ["Toonami", "24/7 Cartoons and Anime"],
-        ["Channel Name", "Channel Description"],
         ["Channel Name", "Channel Description"],
     ];
     return chinfodb;
