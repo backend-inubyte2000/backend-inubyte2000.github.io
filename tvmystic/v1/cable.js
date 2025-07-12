6ee98b01c9554954173e2f0c2eddf98510cb6694
inubyte2000 <inubyte2000@yahoo.com>
2025-07-12 14:01:28 -0400

    Add files via upload

---
+++ tvmystic/v1/cable.js
@@ -0,0 +1,31 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-inubyte2000.github.io/ieyasu/v0/ch2.gif"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch3.gif"],
+        ["index2.html#http://v1.weatherscan.net/", "https://backend-inubyte2000.github.io/ieyasu/v0/ch4.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-inubyte2000.github.io/ieyasu/v0/ch7.gif"],
+        ["index3.html#", "nochan.gif"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Toonami", "24/7 Cartoons and Anime"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
